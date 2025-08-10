40f4478f81d9702e345704fa9cfb990a20d8e0e1
Evstifeev Roman <someuniquename@gmail.com>
2025-08-10 22:26:57 +0500

    test_cli.py - add test for libx264 encoding

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,19 +1,36 @@
-from typer.testing import CliRunner
+import typer.testing
+import enjam.main
 
-from enjam.main import app
-
-runner = CliRunner()
+runner = typer.testing.CliRunner()
 
 
-def test_convert_all_mkv(tmp_path):
+def test_convert_all_mkv_vcodec_copy(tmp_path):
     """ Convert all mkv files in test directory. """
-    result = runner.invoke(app, [
+    result = runner.invoke(enjam.main.app, [
         "--pattern", "*.mkv",
         # "--no-write-log",
         # "--no-skip-errors"
         "--verbose",
         "--vcodec", 'copy',
-        "--dst", str(tmp_path)
+        "--dst", str(tmp_path)  # write files to temporary dir
+    ], catch_exceptions=False)
+    print(result.stdout)
+    assert result.exit_code == 0
+    # 64pix.mkv should be processed successfully
+    assert "Successfully processed 1 files" in result.stdout
+    # Two empty files should report error
+    assert "Failed to process 2 files" in result.stdout
+
+
+def test_convert_all_mkv_vcodec_libx264(tmp_path):
+    """ Convert all mkv files in test directory. """
+    result = runner.invoke(enjam.main.app, [
+        "--pattern", "*.mkv",
+        # "--no-write-log",
+        # "--no-skip-errors"
+        "--verbose",
+        "--vcodec", 'libx264',
+        "--dst", str(tmp_path)  # write files to temporary dir
     ], catch_exceptions=False)
     print(result.stdout)
     assert result.exit_code == 0
@@ -24,14 +41,17 @@ def test_convert_all_mkv(tmp_path):
 
 
 def test_wrong_vbitrate_multiplier(tmp_path):
-    result = runner.invoke(app, [
-        "--pattern", "64pix.mkv",
+    """
+    When incorrect vbitrate multiplier is given, file should not be processed.
+    """
+    result = runner.invoke(enjam.main.app, [
+        "--pattern", "64pix.mkv",  # convert single file
         # "--no-write-log",
         "--no-skip-errors",
         "--vbitrate=x24g",  # NOTE: WRONG FORMAT
         "--verbose",
         "--vcodec", 'copy',
-        "--dst", str(tmp_path)
+        "--dst", str(tmp_path)  # write files to temporary dir
     ], catch_exceptions=True)
     print(result.stdout)
     assert result.exit_code == 2
@@ -40,14 +60,17 @@ def test_wrong_vbitrate_multiplier(tmp_path):
 
 
 def test_correct_vbitrate_multiplier(tmp_path):
-    result = runner.invoke(app, [
-        "--pattern", "64pix.mkv",
+    """
+    When correct vbitrate multiplier is given, file should be processed.
+    """
+    result = runner.invoke(enjam.main.app, [
+        "--pattern", "64pix.mkv",  # convert single file
         # "--no-write-log",
         "--no-skip-errors",
-        "--vbitrate=x24k",
+        "--vbitrate=x24k",  # set output vbitrate to 64*64*24000
         "--verbose",
         "--vcodec", 'copy',
-        "--dst", str(tmp_path)
+        "--dst", str(tmp_path)  # write files to temporary dir
     ], catch_exceptions=True)
     print(result.stdout)
     assert result.exit_code == 0
